6c0ed0f5d2898e6a077b5c95b0ddddb0c2f53797
Stephen Krider <skrider@berkeley.edu>
2022-04-18 14:45:32 -0700

    done with directory rearranging

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,1 @@
-(("default" . ((user-emacs-directory . "~/doom-emacs")))
- ("sp" . ((user-emacs-directory . "~/spacemacs")))
- ("dm" . ((user-emacs-directory . "~/doom-emacs"))))
+(("default" . ((user-emacs-directory . "~/.doom-emacs"))))
