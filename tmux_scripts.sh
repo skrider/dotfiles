c5f86d7aa5bb4d8312e5c4307245445b4509a5d5
skrider <skrider@berkeley.edu>
2022-09-26 23:40:40 -0700

    revert changes to tmux_scripts

--- tmux_scripts.sh
+++ tmux_scripts.sh
@@ -102,6 +102,232 @@ _maximize_pane() {
   fi
 }
 
+_toggle_mouse() {
+  old=$(tmux show -gv mouse)
+  new=""
+
+  if [ "$old" = "on" ]; then
+    new="off"
+  else
+    new="on"
+  fi
+
+  tmux set -g mouse $new
+}
+
+_battery_info() {
+  count=0
+  charge=0
+  case "$_uname_s" in
+    *Darwin*)
+      while IFS= read -r line; do
+        [ -z "$line" ] && continue
+        discharging=$(printf '%s' "$line" | grep -qi "discharging" && echo "true" || echo "false")
+        percentage=$(printf '%s' "$line" | grep -E -o '[0-9]+%' || echo "0%")
+        charge=$(awk -v charge="$charge" -v percentage="${percentage%%%}" 'BEGIN { print charge + percentage / 100 }')
+        count=$((count + 1))
+      done  << EOF
+$(pmset -g batt | grep 'InternalBattery')
+EOF
+      ;;
+    *Linux*)
+      while IFS= read -r batpath; do
+        [ -z "$batpath" ] && continue
+        grep -i -q device "$batpath/scope" 2> /dev/null && continue
+
+        discharging=$(grep -qi "discharging" "$batpath/status" && echo "true" || echo "false")
+        bat_capacity="$batpath/capacity"
+        if [ -r "$bat_capacity" ]; then
+          charge=$(awk -v charge="$charge" -v capacity="$(cat "$bat_capacity")" 'BEGIN { print charge + (capacity > 100 ? 100 : capacity) / 100 }')
+        else
+          bat_energy_full="$batpath/energy_full"
+          bat_energy_now="$batpath/energy_now"
+          if [ -r "$bat_energy_full" ] && [ -r "$bat_energy_now" ]; then
+            charge=$(awk -v charge="$charge" -v energy_now="$(cat "$bat_energy_now")" -v energy_full="$(cat "$bat_energy_full")" 'BEGIN { print charge + energy_now / energy_full }')
+          fi
+        fi
+        count=$((count + 1))
+      done  << EOF
+$(find /sys/class/power_supply -maxdepth 1 -iname '*bat*')
+EOF
+      ;;
+    *CYGWIN*|*MSYS*|*MINGW*)
+      while IFS= read -r line; do
+        [ -z "$line" ] && continue
+        discharging=$(printf '%s' "$line" | awk '{ s = ($1 == 1) ? "true" : "false"; print s }')
+        charge=$(printf '%s' "$line" | awk -v charge="$charge" '{ print charge + $2 / 100 }')
+        count=$((count + 1))
+      done  << EOF
+$(wmic path Win32_Battery get BatteryStatus, EstimatedChargeRemaining 2> /dev/null | tr -d '\r' | tail -n +2 || true)
+EOF
+      ;;
+    *OpenBSD*)
+      for batid in 0 1 2; do
+        sysctl -n "hw.sensors.acpibat$batid.raw0" 2>&1 | grep -q 'not found' && continue
+        discharging=$(sysctl -n "hw.sensors.acpibat$batid.raw0" | grep -q 1 && echo "true" || echo "false")
+        if sysctl -n "hw.sensors.acpibat$batid" | grep -q amphour; then
+          charge=$(awk -v charge="$charge" -v remaining="$(sysctl -n hw.sensors.acpibat$batid.amphour3 | cut -d' ' -f1)" -v full="$(sysctl -n hw.sensors.acpibat$batid.amphour0 | cut -d' ' -f1)" 'BEGIN { print charge + remaining / full }')
+        else
+          charge=$(awk -v charge="$charge" -v remaining="$(sysctl -n hw.sensors.acpibat$batid.watthour3 | cut -d' ' -f1)" -v full="$(sysctl -n hw.sensors.acpibat$batid.watthour0 | cut -d' ' -f1)" 'BEGIN { print charge + remaining / full }')
+        fi
+        count=$((count + 1))
+      done
+      ;;
+  esac
+  [ "$count" -ne 0 ] && charge=$(awk -v charge="$charge" -v count="$count" 'BEGIN { print charge / count }') || true
+}
+
+_battery_status() {
+  _battery_info
+  if [ "$charge" = 0 ]; then
+    tmux set -ug '@battery_status'
+    return
+  fi
+
+  battery_status_charging=$1
+  battery_status_discharging=$2
+  if [ x"$discharging" = x"true" ]; then
+    battery_status="$battery_status_discharging"
+  else
+    battery_status="$battery_status_charging"
+  fi
+
+  tmux set -g '@battery_status' "$battery_status"
+}
+
+_battery_bar() {
+  _battery_info
+  if [ "$charge" = 0 ]; then
+    tmux  set -ug '@battery_bar'     \;\
+          set -ug '@battery_hbar'    \;\
+          set -ug '@battery_vbar'    \;\
+          set -ug '@battery_percentage'
+    return
+  fi
+
+  battery_bar_symbol_full=$1
+  battery_bar_symbol_empty=$2
+  battery_bar_length=$3
+  battery_bar_palette=$4
+  battery_hbar_palette=$5
+  battery_vbar_palette=$6
+
+  if [ x"$battery_bar_length" = x"auto" ]; then
+    columns=$(tmux -q display -p '#{client_width}' 2> /dev/null || echo 80)
+    if [ "$columns" -ge 80 ]; then
+      battery_bar_length=10
+    else
+      battery_bar_length=5
+    fi
+  fi
+
+  if echo "$battery_bar_palette" | grep -q -E '^heat|gradient(,[#a-z0-9]{7,9})?$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_bar_palette; unset IFS; set +f; }
+    palette_style=$1
+    battery_bg=${2:-none}
+    [ x"$palette_style" = x"gradient" ] && \
+      palette="196 202 208 214 220 226 190 154 118 82 46"
+    [ x"$palette_style" = x"heat" ] && \
+      palette="243 245 247 144 143 142 184 214 208 202 196"
+
+    palette=$(echo "$palette" | awk -v n="$battery_bar_length" '{ for (i = 0; i < n; ++i) printf $(1 + (i * NF / n))" " }')
+    eval set -- "$palette"
+
+    full=$(awk "BEGIN { printf \"%.0f\", ($charge) * $battery_bar_length }")
+    battery_bar="#[bg=$battery_bg]"
+    # shellcheck disable=SC2046
+    [ "$full" -gt 0 ] && \
+      battery_bar="$battery_bar$(printf "#[fg=colour%s]$battery_bar_symbol_full" $(echo "$palette" | cut -d' ' -f1-"$full"))"
+    # shellcheck disable=SC2046
+    empty=$((battery_bar_length - full))
+    # shellcheck disable=SC2046
+    [ "$empty" -gt 0 ] && \
+      battery_bar="$battery_bar$(printf "#[fg=colour%s]$battery_bar_symbol_empty" $(echo "$palette" | cut -d' ' -f$((full + 1))-$((full + empty))))"
+      eval battery_bar="$battery_bar#[fg=colour\${$((full == 0 ? 1 : full))}]"
+  elif echo "$battery_bar_palette" | grep -q -E '^(([#a-z0-9]{7,9}|none),?){3}$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_bar_palette; unset IFS; set +f; }
+    battery_full_fg=$1
+    battery_empty_fg=$2
+    battery_bg=$3
+
+    full=$(awk "BEGIN { printf \"%.0f\", ($charge) * $battery_bar_length }")
+    [ x"$battery_bg" != x"none" ] && \
+      battery_bar="#[bg=$battery_bg]"
+    #shellcheck disable=SC2046
+    [ "$full" -gt 0 ] && \
+      battery_bar="$battery_bar#[fg=$battery_full_fg]$(printf "%0.s$battery_bar_symbol_full" $(seq 1 "$full"))"
+    empty=$((battery_bar_length - full))
+    #shellcheck disable=SC2046
+    [ "$empty" -gt 0 ] && \
+      battery_bar="$battery_bar#[fg=$battery_empty_fg]$(printf "%0.s$battery_bar_symbol_empty" $(seq 1 "$empty"))" && \
+      battery_bar="$battery_bar#[fg=$battery_empty_fg]"
+  fi
+
+  if echo "$battery_hbar_palette" | grep -q -E '^heat|gradient(,[#a-z0-9]{7,9})?$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_hbar_palette; unset IFS; set +f; }
+    palette_style=$1
+    [ x"$palette_style" = x"gradient" ] && \
+      palette="196 202 208 214 220 226 190 154 118 82 46"
+    [ x"$palette_style" = x"heat" ] && \
+      palette="233 234 235 237 239 241 243 245 247 144 143 142 184 214 208 202 196"
+
+    palette=$(echo "$palette" | awk -v n="$battery_bar_length" '{ for (i = 0; i < n; ++i) printf $(1 + (i * NF / n))" " }')
+    eval set -- "$palette"
+
+    full=$(awk "BEGIN { printf \"%.0f\", ($charge) * $battery_bar_length }")
+    eval battery_hbar_fg="colour\${$((full == 0 ? 1 : full))}"
+  elif echo "$battery_hbar_palette" | grep -q -E '^([#a-z0-9]{7,9},?){3}$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_hbar_palette; unset IFS; set +f; }
+
+    # shellcheck disable=SC2046
+    eval $(awk "BEGIN { printf \"battery_hbar_fg=$%d\", (($charge) - 0.001) * $# + 1 }")
+  fi
+
+  eval set -- "▏ ▎ ▍ ▌ ▋ ▊ ▉ █"
+  # shellcheck disable=SC2046
+  eval $(awk "BEGIN { printf \"battery_hbar_symbol=$%d\", ($charge) * ($# - 1) + 1 }")
+  battery_hbar="#[fg=${battery_hbar_fg?}]${battery_hbar_symbol?}"
+
+  if echo "$battery_vbar_palette" | grep -q -E '^heat|gradient(,[#a-z0-9]{7,9})?$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_vbar_palette; unset IFS; set +f; }
+    palette_style=$1
+    [ x"$palette_style" = x"gradient" ] && \
+      palette="196 202 208 214 220 226 190 154 118 82 46"
+    [ x"$palette_style" = x"heat" ] && \
+      palette="233 234 235 237 239 241 243 245 247 144 143 142 184 214 208 202 196"
+
+    palette=$(echo "$palette" | awk -v n="$battery_bar_length" '{ for (i = 0; i < n; ++i) printf $(1 + (i * NF / n))" " }')
+    eval set -- "$palette"
+
+    full=$(awk "BEGIN { printf \"%.0f\", ($charge) * $battery_bar_length }")
+    eval battery_vbar_fg="colour\${$((full == 0 ? 1 : full))}"
+  elif echo "$battery_vbar_palette" | grep -q -E '^([#a-z0-9]{7,9},?){3}$'; then
+    # shellcheck disable=SC2086
+    { set -f; IFS=,; set -- $battery_vbar_palette; unset IFS; set +f; }
+
+    # shellcheck disable=SC2046
+    eval $(awk "BEGIN { printf \"battery_vbar_fg=$%d\", (($charge) - 0.001) * $# + 1 }")
+  fi
+
+  eval set -- "▁ ▂ ▃ ▄ ▅ ▆ ▇ █"
+  # shellcheck disable=SC2046
+  eval $(awk "BEGIN { printf \"battery_vbar_symbol=$%d\", ($charge) * ($# - 1) + 1 }")
+  battery_vbar="#[fg=${battery_vbar_fg?}]${battery_vbar_symbol?}"
+
+  battery_percentage="$(awk "BEGIN { printf \"%.0f%%\", ($charge) * 100 }")"
+
+  tmux  set -g '@battery_status' "$battery_status" \;\
+        set -g '@battery_bar' "$battery_bar" \;\
+        set -g '@battery_hbar' "$battery_hbar" \;\
+        set -g '@battery_vbar' "$battery_vbar" \;\
+        set -g '@battery_percentage' "$battery_percentage"
+}
+
 _pane_info() {
   pane_pid="$1"
   pane_tty="${2##/dev/}"
