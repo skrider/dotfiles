1675d00f12c2c613b53af4c93bcbcc7192f1490d
skrider <skrider@berkeley.edu>
2022-10-05 14:48:35 -0700

    eval rm in change_theme

--- bin/change_theme.sh
+++ bin/change_theme.sh
@@ -24,7 +24,7 @@ for (( i = 0; i < $(cat $config_file | jq ".programs | keys | length"); i++ )); 
     source_file=$(cat $config_file | jq -r ".programs[$program] | .source_dir + \"/\" + .theme_name_mapping.$new_theme")
     dest_file=$(cat $config_file | jq -r ".programs[$program] | .destination_file")
     echo Linking to $source_file from $dest_file
-    rm $dest_file
+    eval rm $dest_file
     # unsafe, allows for code injection
     eval ln -s $source_file $dest_file
 done
