c61064ce77605094d005be2725b0c6b837c67b7e
skrider <skrider@berkeley.edu>
2022-10-06 17:42:26 -0700

    improve facebook path picker tmux integration

--- bin/tmux_scripts.sh
+++ bin/tmux_scripts.sh
@@ -1,7 +1,16 @@
 _fpp() {
 	tmux capture-pane -J -S - -E - -b "fpp-$1" -t "$1"
-	tmux popup "tmux show-buffer -b fpp-$1 | fpp || true"
+	tmux popup \
+"tmux show-buffer -b fpp-$1 |"\
+"fpp -c 'source /home/sk/.dotfiles/bin/tmux_scripts.sh ; _lambda ' --no-file-checks || true"
 	tmux delete-buffer -b "fpp-$1"
+}
+
+_lambda() {
+    # remove leading junk that fpp adds
+    echo $@ | sd '^\./+' '' | xclip -i
+    # kill popup forcefully as fpp hangs around
+    tmux popup -C
 }
 
 _urlview() {
@@ -9,3 +18,4 @@ _urlview() {
 	tmux popup "tmux show-buffer -b urlview-$1 | urlview || true"
 	tmux delete-buffer -b "urlview-$1"
 }
+
