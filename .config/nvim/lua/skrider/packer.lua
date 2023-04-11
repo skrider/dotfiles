0fa38745fe4cecb0f0876382ea9b289df8482509
skrider <skrider@berkeley.edu>
2023-04-11 15:19:32 -0700

    stuff

--- .config/nvim/lua/skrider/packer.lua
+++ .config/nvim/lua/skrider/packer.lua
@@ -47,6 +47,7 @@ return require('packer').startup(function(use)
     use("mbbill/undotree")
     use("tpope/vim-fugitive")
     use("nvim-treesitter/nvim-treesitter-context");
+    use("junegunn/vim-easy-align")
 
     use {
         'VonHeikemen/lsp-zero.nvim',
